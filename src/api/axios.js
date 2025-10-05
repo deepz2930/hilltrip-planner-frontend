9c2930786c8aa103c09191a42ba0bba5eae6ea9a
deepz2930 <111347080+deepz2930@users.noreply.github.com>
2025-10-05 19:37:33 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api'
+const API_BASE = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000/api'
 const instance = axios.create({ baseURL: API_BASE })
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
